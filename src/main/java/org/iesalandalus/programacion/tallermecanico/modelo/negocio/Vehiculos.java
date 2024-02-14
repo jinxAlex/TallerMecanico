838e9ea34d058d1d981b8b42a07ed56227c0acf4
Alejandro <alexfern@outlook.es>
2024-02-14 14:36:59 +0100

    Modificado Vehiculos

--- src/main/java/org/iesalandalus/programacion/tallermecanico/modelo/negocio/Vehiculos.java
+++ src/main/java/org/iesalandalus/programacion/tallermecanico/modelo/negocio/Vehiculos.java
@@ -8,36 +8,35 @@ import org.iesalandalus.programacion.tallermecanico.modelo.dominio.Cliente;
 import org.iesalandalus.programacion.tallermecanico.modelo.dominio.Vehiculo;
 
 public class Vehiculos {
-    private Collection<Vehiculo> coleccionVehiculos;
+    private List<Vehiculo> coleccionVehiculos;
 
-    public Vehiculos(){
+    public Vehiculos() {
         coleccionVehiculos = new ArrayList<>();
     }
 
-    public ArrayList<Vehiculo> get(){
+    public ArrayList<Vehiculo> get() {
         return new ArrayList<>(coleccionVehiculos);
     }
 
     public void insertar(Vehiculo vehiculo) throws OperationNotSupportedException {
         Objects.requireNonNull(vehiculo, "No se puede insertar un vehículo nulo.");
-        if(coleccionVehiculos.contains(vehiculo)){
+        if (coleccionVehiculos.contains(vehiculo)) {
             throw new OperationNotSupportedException("Ya existe un vehículo con esa matrícula.");
         }
         coleccionVehiculos.add(vehiculo);
     }
 
-    public Vehiculo buscar(Vehiculo vehiculo){
+    public Vehiculo buscar(Vehiculo vehiculo) {
         Objects.requireNonNull(vehiculo, "No se puede buscar un vehículo nulo.");
-        Vehiculo vehiculoBuscado = null;
-        if (coleccionVehiculos.contains(vehiculo)) {
-            vehiculoBuscado = vehiculo;
-        }
-        return vehiculoBuscado;
+        int indice = coleccionVehiculos.indexOf(vehiculo);
+
+
+        return (indice == -1) ? null : coleccionVehiculos.get(indice);
     }
 
     public void borrar(Vehiculo vehiculo) throws OperationNotSupportedException {
         Objects.requireNonNull(vehiculo, "No se puede borrar un vehículo nulo.");
-        if(!coleccionVehiculos.contains(vehiculo)){
+        if (!coleccionVehiculos.contains(vehiculo)) {
             throw new OperationNotSupportedException("No existe ningún vehículo con esa matrícula.");
         }
         Iterator<Vehiculo> iterator = coleccionVehiculos.iterator();
