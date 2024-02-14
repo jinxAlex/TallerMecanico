001d7e66452188f395d9b130502c01ad312f1b1f
Alejandro <alexfern@outlook.es>
2024-02-14 14:35:44 +0100

    Finalizado Modelo

--- src/main/java/org/iesalandalus/programacion/tallermecanico/modelo/Modelo.java
+++ src/main/java/org/iesalandalus/programacion/tallermecanico/modelo/Modelo.java
@@ -18,61 +18,62 @@ public class Modelo {
     private Revisiones revisiones;
     private Clientes clientes;
     private Vehiculos vehiculos;
-    public Modelo(){
+
+    public Modelo() {
         comenzar();
     }
 
-    public void comenzar(){
+    public void comenzar() {
         revisiones = new Revisiones();
         clientes = new Clientes();
         vehiculos = new Vehiculos();
     }
 
-    public void terminar(){
+    public void terminar() {
         System.out.println("El modelo ha terminado");
     }
 
-    public void insertar(Cliente cliente) throws OperationNotSupportedException{
+    public void insertar(Cliente cliente) throws OperationNotSupportedException {
         clientes.insertar(new Cliente(cliente));
     }
 
-    public void insertar(Vehiculo vehiculo) throws OperationNotSupportedException{
+    public void insertar(Vehiculo vehiculo) throws OperationNotSupportedException {
         vehiculos.insertar(vehiculo);
     }
 
-    public void insertar(Revision revision) throws OperationNotSupportedException{
-        revisiones.insertar(new Revision(clientes.buscar(revision.getCliente()),vehiculos.buscar(revision.getVehiculo()),revision.getFechaInicio()));
+    public void insertar(Revision revision) throws OperationNotSupportedException {
+        revisiones.insertar(new Revision(clientes.buscar(revision.getCliente()), vehiculos.buscar(revision.getVehiculo()), revision.getFechaInicio()));
     }
 
-    public Cliente buscar(Cliente cliente){
+    public Cliente buscar(Cliente cliente) {
         return new Cliente(clientes.buscar(cliente));
     }
 
-    public Vehiculo buscar(Vehiculo vehiculo){
+    public Vehiculo buscar(Vehiculo vehiculo) {
         return vehiculos.buscar(vehiculo);
     }
 
-    public Revision buscar(Revision revision){
+    public Revision buscar(Revision revision) {
         return new Revision(revisiones.buscar(revision));
     }
 
-    public boolean modificar(Cliente cliente, String nombre, String telefono) throws OperationNotSupportedException{
+    public boolean modificar(Cliente cliente, String nombre, String telefono) throws OperationNotSupportedException {
         return clientes.modificar(cliente, nombre, telefono);
     }
 
-    public void anadirHoras(Revision revision, int horas) throws OperationNotSupportedException{
+    public void anadirHoras(Revision revision, int horas) throws OperationNotSupportedException {
         revisiones.anadirHoras(revision, horas);
     }
 
-    public void anadirPrecioMaterial(Revision revision, float precioMaterial) throws OperationNotSupportedException{
+    public void anadirPrecioMaterial(Revision revision, float precioMaterial) throws OperationNotSupportedException {
         revisiones.anadirPrecioMaterial(revision, precioMaterial);
     }
 
-    public void cerrar(Revision revision, LocalDate fechaFin) throws OperationNotSupportedException{
+    public void cerrar(Revision revision, LocalDate fechaFin) throws OperationNotSupportedException {
         revisiones.cerrar(revision, fechaFin);
     }
 
-    public void borrar(Cliente cliente) throws OperationNotSupportedException{
+    public void borrar(Cliente cliente) throws OperationNotSupportedException {
         Iterator<Revision> iterator = revisiones.get(cliente).iterator();
         while (iterator.hasNext()) {
             Revision revisionActual = iterator.next();
@@ -81,49 +82,49 @@ public class Modelo {
         clientes.borrar(cliente);
     }
 
-    public void borrar(Vehiculo vehiculo) throws OperationNotSupportedException{
+    public void borrar(Vehiculo vehiculo) throws OperationNotSupportedException {
         ArrayList<Revision> coleccionVehiculos = revisiones.get(vehiculo);
-        for(Revision revision: coleccionVehiculos){
+        for (Revision revision : coleccionVehiculos) {
             revisiones.borrar(revision);
         }
         vehiculos.borrar(vehiculo);
     }
 
-    public void borrar(Revision revision) throws OperationNotSupportedException{
+    public void borrar(Revision revision) throws OperationNotSupportedException {
         revisiones.borrar(revision);
     }
 
-    public ArrayList<Cliente> getClientes(){
+    public ArrayList<Cliente> getClientes() {
         ArrayList<Cliente> coleccionClientes = new ArrayList<>();
-        for(Cliente cliente: clientes.get()){
+        for (Cliente cliente : clientes.get()) {
             coleccionClientes.add(new Cliente(cliente));
         }
         return coleccionClientes;
     }
 
-    public ArrayList<Vehiculo> getVehiculos(){
+    public ArrayList<Vehiculo> getVehiculos() {
         return new ArrayList<Vehiculo>(vehiculos.get());
     }
 
-    public ArrayList<Revision> getRevisiones(){
+    public ArrayList<Revision> getRevisiones() {
         ArrayList<Revision> coleccionRevisiones = new ArrayList<>();
-        for(Revision revision: revisiones.get()){
+        for (Revision revision : revisiones.get()) {
             coleccionRevisiones.add(new Revision(revision));
         }
         return coleccionRevisiones;
     }
 
-    public ArrayList<Revision> getRevisiones(Cliente cliente){
+    public ArrayList<Revision> getRevisiones(Cliente cliente) {
         ArrayList<Revision> coleccionRevisionesCliente = new ArrayList<>();
-        for(Revision revision: revisiones.get(cliente)){
+        for (Revision revision : revisiones.get(cliente)) {
             coleccionRevisionesCliente.add(new Revision(revision));
         }
         return coleccionRevisionesCliente;
     }
 
-    public ArrayList<Revision> getRevisiones(Vehiculo vehiculo){
+    public ArrayList<Revision> getRevisiones(Vehiculo vehiculo) {
         ArrayList<Revision> coleccionRevisionesVehiculo = new ArrayList<>();
-        for(Revision revision: revisiones.get(vehiculo)){
+        for (Revision revision : revisiones.get(vehiculo)) {
             coleccionRevisionesVehiculo.add(new Revision(revision));
         }
         return coleccionRevisionesVehiculo;
