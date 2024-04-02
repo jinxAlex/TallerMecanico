93136907e48aec2c9d36c86405f75995e8b1a315
Alejandro <alexfern@outlook.es>
2024-04-02 14:00:26 +0200

    Finalizado Main

--- src/main/java/org/iesalandalus/programacion/tallermecanico/Main.java
+++ src/main/java/org/iesalandalus/programacion/tallermecanico/Main.java
@@ -1,6 +1,7 @@
 package org.iesalandalus.programacion.tallermecanico;
 
 import org.iesalandalus.programacion.tallermecanico.controlador.Controlador;
+import org.iesalandalus.programacion.tallermecanico.controlador.IControlador;
 import org.iesalandalus.programacion.tallermecanico.modelo.FabricaModelo;
 import org.iesalandalus.programacion.tallermecanico.modelo.Modelo;
 import org.iesalandalus.programacion.tallermecanico.modelo.negocio.FabricaFuenteDatos;
@@ -11,7 +12,7 @@ public class Main {
     public static void main(String[] args) {
         Vista vista = FabricaVista.TEXTO.crear();
         Modelo modelo = FabricaModelo.CASCADA.crear(FabricaFuenteDatos.MEMORIA);
-        Controlador controlador = new Controlador(modelo, vista);
+        IControlador controlador = new Controlador(modelo, vista);
         controlador.comenzar();
     }
 }
