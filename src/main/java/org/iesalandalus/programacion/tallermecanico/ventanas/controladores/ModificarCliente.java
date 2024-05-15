4e1ed6e1c3095c6fc0b54f8943f50173fc5885e5
Alejandro <alexfern@outlook.es>
2024-05-15 14:37:53 +0200

    0.5

---
+++ src/main/java/org/iesalandalus/programacion/tallermecanico/ventanas/controladores/ModificarCliente.java
@@ -0,0 +1,29 @@
+package org.iesalandalus.programacion.tallermecanico.ventanas.controladores;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import org.iesalandalus.programacion.tallermecanico.ventanas.utilidades.Controlador;
+
+public class ModificarCliente extends Controlador {
+    @FXML
+    private TextField tfDni;
+
+    @FXML
+    private TextField tfNombre;
+
+    @FXML
+    private TextField tfTlf;
+
+    @FXML
+    void cancelarAccion(ActionEvent event) {
+
+    }
+
+    @FXML
+    void crearCliente(ActionEvent event) {
+
+    }
+
+
+}
